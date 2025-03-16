ea87d1c51664235465e3da2df70a6eb248c02410
robertocastrillo <144861745+robertocastrillo@users.noreply.github.com>
2025-03-15 21:04:40 -0600

    Trabajo en el BE

---
+++ Backend/Entidades/Entity/Opcion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Entidades
+{
+   public class Opcion
+    {
+        public int IdOpcion { get; set; }
+        public string Descripcion { get; set; }
+        public bool Condicion { get; set; } 
+        public int IdPregunta { get; set; }
+    }
+}
