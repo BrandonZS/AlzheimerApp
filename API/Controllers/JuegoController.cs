19cbb06e2f2838f2a8bf64db3edf70d8c54b592b
BrandonZS <124947206+BrandonZS@users.noreply.github.com>
2025-03-20 18:12:10 -0600

    Funcionalidad de insertar Juego

--- API/Controllers/JuegoController.cs
+++ API/Controllers/JuegoController.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using Backend.Entidades;
+using Backend.Logica.Juego;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace API.Controllers
 {
-    public class JuegoController : Controller
+    public class JuegoController : ApiController
     {
-
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/juego/insertarjuego")]
+        public ResInsertarJuego insertarUsuario(ReqInsertarJuego req) //INVESTIGAR: Recibir y retornar HTTP
+        {
+            return new LogJuego().insertarJuego(req);
+        }
     }
 }
---
+++ Backend/Logica/juego/ValidarJuego.cs
@@ -0,0 +1,47 @@
+﻿using Backend.Entidades;
+using Backend.Entidades.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Logica.Juego
+{
+    public static class ValidarJuego
+    {
+        public static List<Error> validarJuego(ReqInsertarJuego req)
+        {
+            List<Error> errores = new List<Error>();
+            Error error = new Error();
+
+            if (req == null)
+            {
+                error.idError = (int)CatalogoErrores.requestNull;
+                error.error = "Request null";
+                errores.Add(error);
+            }
+            else
+            {
+                if (String.IsNullOrEmpty(req.nombre))
+                {
+                    //Cambiar nombre de los errores   <<<--------------
+                    error.idError = (int)ErroresBrandon.nombreJuegoNuloVacio;
+                    error.error = "Falta el nombre del usuario";
+                    errores.Add(error);
+                }
+
+                if (req.idUsuario < 1 )
+                {
+                    //Cambiar nombre de los errores  <<<--------------
+                    error.idError = (int)ErroresBrandon.usuarioInvalido;
+                    error.error = "Falta el correo electronico del usuario";
+                    errores.Add(error);
+                }
+
+            }
+            return errores;
+        }
+
+    }
+}
